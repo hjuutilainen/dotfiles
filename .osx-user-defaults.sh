8f4ef20795d5aeb3ed350e6e66b31a7bf7f75c31
Hannes Juutilainen <hjuutilainen@mac.com>
2012-09-30 18:28:10 +0300

    Name Mangler settings

--- .osx-user-defaults.sh
+++ .osx-user-defaults.sh
@@ -273,6 +273,26 @@ defaults write com.alice.mac.go2shell customCommand -string "cd %PATH%; pwd"
 
 
 # ==============================================
+# Name Mangler
+# ==============================================
+
+# Concise list
+defaults write com.manytricks.NameMangler "List Style" -int 0
+
+# No icon previews
+defaults write com.manytricks.NameMangler "Show Icon Previews" -bool false
+
+# Quit when window closed
+defaults write com.manytricks.NameMangler "Quit When Closed" -bool true
+
+# Don't clear the list after processing
+defaults write com.manytricks.NameMangler Repopulate -bool false
+
+# No confirmation, just do it
+defaults write com.manytricks.NameMangler Chicken -bool false
+
+
+# ==============================================
 # Finder
 # ==============================================
 
