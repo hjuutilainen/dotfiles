a4942e62c7ce3d7bd9c01bf41aec6c6f4b01d3ad
Hannes Juutilainen <hjuutilainen@mac.com>
2014-05-02 10:51:25 +0300

    Updated Activity Monitor section for Mavericks

--- bin/osx-user-defaults.sh
+++ bin/osx-user-defaults.sh
@@ -176,14 +176,39 @@ defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
 defaults write com.apple.ActivityMonitor IconType -int 5
 
 # Show all processes
-defaults write com.apple.ActivityMonitor ShowCategory -int 0
+defaults write com.apple.ActivityMonitor ShowCategory -int 100
 
-# Sort by CPU usage
+# Mountain Lion: Sort by CPU usage
 defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
 defaults write com.apple.ActivityMonitor SortDirection -int 0
 
+# Mavericks: Add the "% CPU" column to the Disk and Network tabs
+defaults write com.apple.ActivityMonitor "UserColumnsPerTab v4.0" -dict \
+    '0' '( Command, CPUUsage, CPUTime, Threads, IdleWakeUps, PID, UID )' \
+    '1' '( Command, anonymousMemory, Threads, Ports, PID, UID, ResidentSize )' \
+    '2' '( Command, PowerScore, 12HRPower, AppSleep, graphicCard, UID )' \
+    '3' '( Command, bytesWritten, bytesRead, Architecture, PID, UID, CPUUsage )' \
+    '4' '( Command, txBytes, rxBytes, txPackets, rxPackets, PID, UID, CPUUsage )'
+
+# Mavericks: Sort by CPU usage in Disk and Network tabs
+defaults write com.apple.ActivityMonitor UserColumnSortPerTab -dict \
+    '0' '{ direction = 0; sort = CPUUsage; }' \
+    '1' '{ direction = 0; sort = ResidentSize; }' \
+    '2' '{ direction = 0; sort = 12HRPower; }' \
+    '3' '{ direction = 0; sort = CPUUsage; }' \
+    '4' '{ direction = 0; sort = CPUUsage; }'
+
 # Select the Network tab
 defaults write com.apple.ActivityMonitor SelectedTab -int 4
+
+# Update Frequency: Often (2 sec)
+defaults write com.apple.ActivityMonitor UpdatePeriod -int 2
+
+# Mavericks: Show Data in the Disk graph (instead of IO)
+defaults write com.apple.ActivityMonitor DiskGraphType -int 1
+
+# Mavericks: Show Data in the Network graph (instead of packets)
+defaults write com.apple.ActivityMonitor NetworkGraphType -int 1
 
 
 # ==============================================
