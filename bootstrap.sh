b049797c5bb5a0cc4dc29a7ebabb286337c01795
Hannes Juutilainen <hjuutilainen@mac.com>
2023-07-17 14:27:17 +0300

    Update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # ==============================================================
 # This script is originally written by:
 # Mathias Bynens, https://github.com/mathiasbynens/dotfiles
 # ==============================================================
 
-cd "$(dirname "$0")"
-#git pull
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin main;
+
 function doIt() {
-    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
-    chflags hidden ~/bin
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude ".osx" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE-MIT.txt" \
+		-avh --no-perms . ~;
+	source ~/.bash_profile;
 }
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-    doIt
+	doIt;
 else
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    doIt
-    fi
-fi
-unset doIt
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt;
+	fi;
+fi;
+unset doIt;
